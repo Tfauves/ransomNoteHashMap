1752df6e74b52d92b41c22909f82b22cd2acaa86
Tfauves <tfauvelle@gmail.com>
2021-07-25 11:23:20 -0500

    refactored checkMagazine to create hashMape from List.

--- src/com/careerDevs/Main.java
+++ src/com/careerDevs/Main.java
@@ -1,28 +1,47 @@
 package com.careerDevs;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	List<String> magazine = new ArrayList<>();
+	List<String> note = new ArrayList<>();
+	magazine.add("this is some text.");
+	magazine.add("meet me at midnight.");
+	note.add("this is more text.");
+	note.add("at midnight.");
+	note.add("this is some extra text.");
 
-        List<String> magazine1 = new ArrayList<>();
-        List<String> note1 = new ArrayList<>();
-        magazine1.add("Heres some text.");
-        note1.add("Here's more text.");
+	checkMagazine(magazine, note);
 
-        checkMagazine(magazine1, note1);
     }
 
-    public static void checkMagazine(List<String> magazine, List<String> note) {
-        String words = "";
-        for (String sentence : magazine) {
-            words += sentence;
-            System.out.println(words);
 
-        }
+    public static void checkMagazine(List<String> magazine, List<String> note) {
+    	HashMap<Integer, String> magMap = new HashMap<>();
+    	HashMap<Integer, String> noteMap = new HashMap<>();
+    	for (int i = 0; i < magazine.size(); i++) {
+    		String magText = magazine.get(i);
+    		magMap.put(i, magText);
+		}
+			System.out.println(magMap);
+
+    	for (int i = 0; i < note.size(); i++) {
+    		String noteText = note.get(i);
+    		noteMap.put(i, noteText);
+		}
+			System.out.println(noteMap);
+
 
 
 
